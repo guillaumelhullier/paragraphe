ee425352b86770a5ae8f6e5fff6c166d592f93b1
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-03-03 19:02:49 +0100

    Style

--- paragraphe/src/components/multiparagraphe/Multiparagraphe.jsx
+++ paragraphe/src/components/multiparagraphe/Multiparagraphe.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Multiparagraphe = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nemo
+        illo iste sequi labore temporibus doloremque doloribus odit. Repellat,
+        voluptates.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dignissimos
+        laboriosam modi sequi soluta non velit nihil repellat est ipsum.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At eos
+        accusamus nihil alias ex laudantium adipisci ea necessitatibus voluptate
+        beatae?
+      </p>
+    </div>
+  );
 };
 
 export default Multiparagraphe;
