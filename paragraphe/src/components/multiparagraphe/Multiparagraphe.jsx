8f51e8cb7efe725829b0a4489690dc87ee7a1b8e
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-03-06 19:07:10 +0100

    style multiparagraphe

--- paragraphe/src/components/multiparagraphe/Multiparagraphe.jsx
+++ paragraphe/src/components/multiparagraphe/Multiparagraphe.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import "./multipagraphe.css";
+import { Link } from "react-router-dom";
+import "./multipagraphe.css";
 
 const Multiparagraphe = () => {
   return (
     <div>
-      <h4>Premier paragraphe</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nemo
-        illo iste sequi labore temporibus doloremque doloribus odit. Repellat,
-        voluptates.
-      </p>
-      <br />
-      <h4>Second paragraphe</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dignissimos
-        laboriosam modi sequi soluta non velit nihil repellat est ipsum.
-      </p>
-      <br />
-      <h4>Troisième paragraphe</h4>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At eos
-        accusamus nihil alias ex laudantium adipisci ea necessitatibus voluptate
-        beatae?
-      </p>
+      <Link to="/" className="suite">
+        <h4 className="suite">Premier paragraphe</h4>
+        <p className="suite">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nemo
+          illo iste sequi labore temporibus doloremque doloribus odit. Repellat,
+          voluptates.
+        </p>
+        <br />
+        <h4 className="suite">Second paragraphe</h4>
+        <p className="suite">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+          dignissimos laboriosam modi sequi soluta non velit nihil repellat est
+          ipsum.
+        </p>
+        <br />
+        <h4>Troisième paragraphe</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. At eos
+          accusamus nihil alias ex laudantium adipisci ea necessitatibus
+          voluptate beatae?
+        </p>
+      </Link>
     </div>
   );
 };
--- paragraphe/src/components/paragraphe/Paragraphe.jsx
+++ paragraphe/src/components/paragraphe/Paragraphe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./pargraphe.css";
 
 const Paragraphe = () => {
   return (
